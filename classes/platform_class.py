a00ca68cdb80ef2e5d0d9bfd4fbe174e8df80a8c
victorbjo <victor.bjoerholm@gmail.com>
2022-07-27 14:52:19 +0200

    Added platform class

--- classes/platform_class.py
+++ classes/platform_class.py
@@ -3,3 +3,9 @@ WIDTH = 800
 ACC = 2
 FRIC = -1
 FPS = 60
+import pygame
+class platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((WIDTH, 20))
+        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
